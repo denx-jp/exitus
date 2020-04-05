7967f01db011d60c6e4be14d87f8a74366dde41a
unknown <0622okakyo@gmail.com>
2020-04-05 14:49:15 +0900

    フロント設計中

---
+++ frontend/components/base/grid/BaseLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex ma-4">
+      <div class="w-full">
+        <slot name="left-window"/>
+      </div>
+      <div>
+        <slot name="right-window"/>
+      </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name:"BaseGrid"
+})
+</script>
